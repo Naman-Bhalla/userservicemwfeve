bb9f0ea0fb78dacc1e6c57a56652ea225aab0697
Naman Bhalla <naman@Namans-MacBook-Pro.local>
2024-02-12 23:13:06 +0530

    Done with authentication

--- src/main/java/com/scaler/userservicemwfeve/security/models/CustomUserDetails.java
+++ src/main/java/com/scaler/userservicemwfeve/security/models/CustomUserDetails.java
@@ -20,7 +20,15 @@ public class CustomUserDetails implements UserDetails {
     private boolean accountNonLocked;
     private boolean credentialsNonExpired;
     private boolean enabled;
+    private Long userId;
 
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
 
     public CustomUserDetails() {}
 
@@ -32,6 +40,7 @@ public class CustomUserDetails implements UserDetails {
         this.credentialsNonExpired = true;
         this.password = user.getHashedPassword();
         this.username = user.getEmail();
+        this.userId = user.getId();
 
         List<CustomGrantedAuthority> grantedAuthorities = new ArrayList<>();
 
